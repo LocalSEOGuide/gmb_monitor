7c7767a92f34369acec95ce8133c3647f1f6acea
Tingbin Huang <tingbinhuang@Tims-MacBook-Pro.local>
2022-12-01 16:09:58 -0800

    Added class

--- monitor.py
+++ monitor.py
@@ -1,3 +1,6 @@
+import json
+import os.path
+
 # Setup Gdrive Files
 ## create reporting setup (user input)
 
@@ -64,3 +67,75 @@ class MonReport:
 
     def output_reporting_record(self):
         return
+
+
+class GmbLocation: 
+
+    def __init__(self,locationList):
+        
+        self.output = self.readFile()
+        self.index = len(self.output)
+        self.input = locationList 
+
+    def addLocation(self): 
+
+        for i in range(len(self.input)):
+            temp = {} 
+            temp['brand_query'] = self.input[i]['brand_term']
+            temp['group_id'] = self.index + 1 
+            self.output.append(temp) 
+
+        print(self.output)
+        self.saveToFile()
+
+    def readFile(self): 
+        if os.path.exists('sample.json'):
+            with open('sample.json', 'r') as openfile:
+                return json.load(openfile)
+        else:
+            return []
+
+    def saveToFile(self):
+        with open("output.json", "w") as outfile:
+            json.dump(self.output, outfile)
+        print("Save output to file!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
