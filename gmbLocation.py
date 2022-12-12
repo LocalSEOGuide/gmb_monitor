9928503c4160fae654ce45f102ec197fbe989f80
Tingbin Huang <tingbinhuang@Tims-MacBook-Pro.local>
2022-12-11 22:54:12 -0800

    Updated file

--- gmbLocation.py
+++ gmbLocation.py
@@ -2,10 +2,11 @@ class GmbLocation:
 
     def __init__(self,locationList):
         
-        self.output = self.readFile()
+        self.output = self.readFile() # scan json file from local machine
         self.index = len(self.output)
         self.input = locationList 
 
+    # function to add location inputs. 
     def addLocation(self): 
 
         for i in range(len(self.input)):
@@ -13,10 +14,10 @@ class GmbLocation:
             temp['brand_query'] = self.input[i]['brand_term']
             temp['group_id'] = self.index + 1 
             self.output.append(temp) 
+        self.saveToFile() # save input as json file to local machine 
 
-        print(self.output)
-        self.saveToFile()
-
+    # function to scan json file from local machine and return contents 
+    # it will be called every time the class initiated 
     def readFile(self): 
         if os.path.exists('output.json'):
             with open('output.json', 'r') as openfile:
@@ -24,6 +25,7 @@ class GmbLocation:
         else:
             return []
 
+    # function to save new list into json file 
     def saveToFile(self):
         with open("output.json", "w") as outfile:
             json.dump(self.output, outfile)
