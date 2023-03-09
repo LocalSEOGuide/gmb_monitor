6ebac22197997fdff7789a83a680e5d27941484a
Tingbin Huang <tingbinhuang@Tims-MacBook-Pro.local>
2023-03-08 23:14:41 -0800

    Updated test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+import main as m
+from gmbLocation import GmbLocation as gmbL
+
+
 LOCATION_LIST = [
     'audi north austin',
     'audi south austin',
@@ -5,20 +9,11 @@ LOCATION_LIST = [
     'austin porsche'
 ]
 
-
 def test_adding_locations(location_list=None):
     if not location_list:
         location_list = LOCATION_LIST
-
-    import main as m
-    from gmbLocation import GmbLocation as gmbL
-
     job = gmbL()
     local_obj = job.readFile()
-
     for loc in location_list:
-        print(loc)
         local_obj = m.updateLocalObject(loc, local_obj)
-
     job.addLocation(local_obj)
-    return
